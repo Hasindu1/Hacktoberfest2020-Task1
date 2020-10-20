577cb23822320c32bdcca4b197bca25777c2cd8b
Hasindu Dahanayake <hasiyadahanayake@gmail.com>
2020-10-21 00:28:08 +0530

    Updated Program.c

--- Program.c
+++ Program.c
@@ -1,18 +1,24 @@
+
 #include <stdio.h>
 
 int main()
 {
-    char name,country,skill;
+    //To store user inputs
+    char name[100];
+    char country[100];
+    char skill;
+    int age;
+
     printf("Welcome to Hacktoberfest 2020\n");
     printf("Enter your name : ");//Get User name
-    scanf("%d",&name);
+    scanf("%[^\n]%*c", name);//Scanning the user input of name including the spaces between 
     printf("Enter your Country of Residance : ");//Get user country
-    scanf("%s",&country);
+    scanf("%[^\n]%*c", country);//Scaning the user input of country considering spaces between 
     printf("Enter your Age : ");//Get user Age
-    printf("%d",&age);
+    scanf("%d",&age);//Scanning the user input of age
     printf("Rate your programmings skills from A-D (A being the highest and D being the lowest : ");//Get user skill level
     scanf(" %c",&skill);
-    scanf("Thank you %c Happy Hacking",country);//Thank the user
+    printf("Thank you %s Happy Hacking",name);//Thank the user
 
     return 0;
 }
